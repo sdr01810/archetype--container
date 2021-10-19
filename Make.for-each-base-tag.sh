78e9db8de5f2f15535010dbe11d7ffd582015b51
Stephen Rogers <stephenr@ossia.com>
2021-10-18 23:43:22 -0400

    Successive refinement

--- Make.for-each-base-tag.sh
+++ Make.for-each-base-tag.sh
@@ -14,7 +14,7 @@ for x1 in $(egrep -v '^\s*(#|$)' Docker.image.base.tag.list || :) ; do
 	x2=${x1#*/} ; x2=${x2//:/-}
 
 	(
-		for g1 in "$@" ; do
+		for g1 in "${@:-check}" ; do
 
 			xx :
 			xx make container_image_base_tag_fq="${x1:?}" "${g1:?}"
@@ -25,6 +25,10 @@ for x1 in $(egrep -v '^\s*(#|$)' Docker.image.base.tag.list || :) ; do
 
 	) |& tee .sb.make.$(timestamp-as-file-name).base-tag.${x2:?}.tty
 done
+
+xx :
+xx make source
+#^-- ensures primary base tag is current
 
 echo 1>&2
 echo 1>&2 "MAKE FOR EACH BASE TAG COMPLETE; EXIT CODE: $?"
